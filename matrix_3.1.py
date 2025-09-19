eee027b725ed51c614d49259b419f3277d266d6b
Jerry Hu <work.jerrywu@gmail.com>
2025-09-19 15:09:36 -0700

    added correlation matrix 3.1

--- matrix_3.1.py
+++ matrix_3.1.py
@@ -3,8 +3,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 data = {
-    'Noise Level / dB': [61.5, 64.9, 65.9, 59.7, 69.8, 69.7, 63.9, 65.9, 64.7, 62.3, 54.8, 54.7],
-    'Distance from the CBD / m': [445.38, 176.67, 0, 342.14, 835.71, 99.89, 399.27, 205.07, 514.47, 895.84, 1278.35, 1600],
+    'Average Noise Level / dB': [61.5, 64.9, 65.9, 59.7, 69.8, 69.7, 63.9, 65.9, 64.7, 62.3, 54.8, 54.7],
+    'Distance / m': [445.38, 176.67, 0, 342.14, 835.71, 99.89, 399.27, 205.07, 514.47, 895.84, 1278.35, 1600],
 }
 
 df = pd.DataFrame(data)
@@ -12,5 +12,5 @@ corr_matrix = df.corr()
 
 plt.figure(figsize=(8, 6))
 sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
-plt.title("Noise Level (dB) vs. Distance from the CBD (m)")
+plt.title("Average Noise Level (dB) vs. Distance from the PLVI (m)")
 plt.show()
